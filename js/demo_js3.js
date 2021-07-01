1fde7a9edb465e08aa5cc37a9cd92bd531ddf79a
lebv28@gmail.com <lebv28@gmail.com>
2021-07-01 22:22:32 +0700

    add

--- js/demo_js3.js
+++ js/demo_js3.js
@@ -2,3 +2,10 @@ var t=document.getElementById("title");
 t.innerText="Nội dung mới này được viết từ js";
 
 var row = document.getElementById(row)
+var ct = "<div class="item">
+                <img src="images/ass3.4.jpg"/>
+                <h1>hip pants 1</h1>
+                <p>bộ thể thao độc đáo. Vô cùng thoáng mát cho ngày hè nóng bức</p>
+                <h2>350.000 VNĐ</h2>
+                <p class="back">MUA NGAY</p>
+                </div> ";
