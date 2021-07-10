2e17d90d62f48f421d23ffd1c4a1e11b23e9275d
lebv28@gmail.com <lebv28@gmail.com>
2021-07-10 11:33:35 +0700

    add

--- js/assignment_js4.js
+++ js/assignment_js4.js
@@ -2,10 +2,22 @@ loadjson();
 
 function loadjson(){
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=hanoi,vietnam&&units=metric&appid=09a71427c59d38d6a34f89b47d75975c",
+        url: "https://api.openweathermap.org/data/2.5/weather",
         method: "GET",
-        success: function (rs){
+        // url có chỉ số: q=hanoi,vietnam&&units=metric&appid=09a71427c59d38d6a34f89b47d75975c có thể viết thành data
+        data:{
+            q:"saigon,vietnam",  // Có thẻ thay đổi các chỉ số này theo API
+            units: "metric",
+            appid: "09a71427c59d38d6a34f89b47d75975c",
 
+        },
+        success: function (rs){
+            $("#temp").text(rs.main.temp);
+            $("#hum").text(rs.main.humidity);
+            $("#wind").text(rs.wind.speed);
+            $("#name").text(rs.name);
+            $("#contry").text(rs.sys.country);
+            $("#des").text(rs.weather[0].description);   
         }
     });
 }
